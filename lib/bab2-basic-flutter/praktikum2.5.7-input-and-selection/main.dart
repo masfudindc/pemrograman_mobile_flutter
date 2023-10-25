c95a2b091bb05b290310ac08d4f8ff94d1226990
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-10-26 00:00:57 +0700

    feat: praktikum2.5.7 input and selection

---
+++ lib/bab2-basic-flutter/praktikum2.5.7-input-and-selection/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh TextField")),
+        body: const TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
+          ),
+        ),
+      ),
+    );
+  }
+}
