9933c05599ea90f7b77fd84d1626ea83c6e02fb3
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-10-25 15:04:19 +0700

    chore: move to folder bab1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pemrograman_mobile_flutter/main.dart';
+import 'package:pemrograman_mobile_flutter/bab1-pengenalan-flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
