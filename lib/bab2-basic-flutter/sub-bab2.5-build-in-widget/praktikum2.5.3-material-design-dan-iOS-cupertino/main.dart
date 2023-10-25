b722f13ba5c1760debf476e1ab6a9548bea30ace
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-10-26 00:36:47 +0700

    refactor: main

--- lib/bab2-basic-flutter/sub-bab2.5-build-in-widget/praktikum2.5.3-material-design-dan-iOS-cupertino/main.dart
+++ lib/bab2-basic-flutter/sub-bab2.5-build-in-widget/praktikum2.5.3-material-design-dan-iOS-cupertino/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
