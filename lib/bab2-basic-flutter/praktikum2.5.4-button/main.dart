6da81aab03b210b97966a1a32d1c4cfb62470178
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-10-25 23:35:59 +0700

    feat: praktikum2.5.4-button

---
+++ lib/bab2-basic-flutter/praktikum2.5.4-button/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+void main() => runApp(const FabWidget());
+class FabWidget extends StatelessWidget {
+  const FabWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          backgroundColor: Colors.pink,
+          child: const Icon(Icons.thumb_up),
+        ),
+      ),
+    );
+  }
+}
