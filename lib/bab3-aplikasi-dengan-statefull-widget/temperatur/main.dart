1df4481295d4311cade21d1595a324dbbc4c957c
Masfudin Dwi Cahyo <masfudindc@gmail.com>
2023-11-22 15:40:43 +0700

    refactor (temperatur): separate widget

--- lib/bab3-aplikasi-dengan-statefull-widget/temperatur/main.dart
+++ lib/bab3-aplikasi-dengan-statefull-widget/temperatur/main.dart
@@ -1,4 +1,8 @@
+// main.dart
 import 'package:flutter/material.dart';
+import 'input.dart';
+import 'result.dart';
+import 'convert.dart';
 
 void main() => runApp(MyApp());
 
@@ -45,52 +49,9 @@ class _TemperatureConverterState extends State<TemperatureConverter> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextField(
-                controller: _celsiusController,
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                decoration: InputDecoration(
-                  labelText: 'Masukkan Suhu (Celsius)',
-                ),
-              ),
-              SizedBox(height: 20),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 200),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('Suhu dalam Kelvin:', style: TextStyle(fontSize: 18)),
-                        Text('Suhu dalam Reamur:', style: TextStyle(fontSize: 18)),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('$_kelvinResult', style: TextStyle(fontSize: 40)),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text('$_reamurResult', style: TextStyle(fontSize: 40)),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Container(
-                margin: EdgeInsets.only(top: 250),
-                child: ElevatedButton(
-                  onPressed: () {
-                    _convertTemperature();
-                  },
-                  child: Text('Konversi Suhu'),
-                ),
-              ),
+              InputSection(controller: _celsiusController),
+              ResultSection(kelvinResult: _kelvinResult, reamurResult: _reamurResult),
+              ButtonSection(onPressed: _convertTemperature),
             ],
           ),
         ),
